ec5856953a59db290f066aa857ac92ad67754c79
turik777 <52492191+turik777@users.noreply.github.com>
2024-04-29 02:46:58 +0300

    Add functionality for display window

--- main.js
+++ main.js
@@ -53,3 +53,15 @@ function displayBooks() {
         tbody.appendChild(read);
     });
 }
+
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("dialog + button");
+const closeButton = document.querySelector("dialog button");
+
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
